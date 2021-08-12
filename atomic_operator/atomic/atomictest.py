75537c136ad89e4c228c464904fa59779cfae4ed
msadministrator <rickardja@live.com>
2021-08-12 16:38:18 -0500

    Updated atomictest class since Atomics repo issues were fixed

--- atomic_operator/atomic/atomictest.py
+++ atomic_operator/atomic/atomictest.py
@@ -44,15 +44,11 @@ class AtomicTest:
     description                                 = attr.ib()
     supported_platforms                         = attr.ib()
     auto_generated_guid                         = attr.ib()
-    executor: typing.List[AtomicExecutor]       = attr.ib()
+    executor                                    = attr.ib()
     input_arguments                             = attr.ib(default=None)
     dependency_executor_name                    = attr.ib(default=None)
     dependencies: typing.List[AtomicDependency] = attr.ib(default=[])
-    # temporary until Atomics are updated - https://github.com/redcanaryco/atomic-red-team/pull/1555
-    get_prereq_command = attr.ib(default=None)
-    prereq_command = attr.ib(default=None)
-    # temporary until Atomics are updated - https://github.com/redcanaryco/atomic-red-team/pull/1555
-    _replacement_strings                        = attr.ib(init=False, default=[
+    _replacement_strings                        = attr.ib(init=False, repr=False, default=[
         '#{{{0}}}',
         '${{{0}}}'
     ])
@@ -66,42 +62,29 @@ class AtomicTest:
                 argument_dict.update({'name': key})
                 temp_list.append(AtomicTestInput(**argument_dict))
             self.input_arguments = temp_list
-        # temporary until fixed by Atomics - https://github.com/redcanaryco/atomic-red-team/pull/1554
-        temp_prereq_command = None
         if self.executor:
-            executor  = []
-            if self.executor.get('name') == 'manual':
-                if not self.executor.get('command'):
-                    self.executor['command'] = ''
-            # Temporary until Atomics are fixed
-            if self.executor.get('cleanup') and not self.executor.get('cleanup_command'):
-                self.executor['cleanup_command'] = self.executor['cleanup']
-                self.executor.pop('cleanup')
-
-            # Temporary until Atomics are fixed - https://github.com/redcanaryco/atomic-red-team/pull/1554
-            if self.executor.get('prereq_command'):
-                temp_prereq_command = self.executor['prereq_command']
-                self.executor.pop('prereq_command')
-            executor.append(AtomicExecutor(**self.executor))
-            self.executor = executor
+            executor_dict = self.executor
+            if executor_dict.get('name') == 'manual':
+                if not executor_dict.get('command'):
+                    executor_dict['command'] = ''
+            self.executor = AtomicExecutor(**executor_dict)
+            executor_dict = None
+        else:
+            self.executor = []
         if self.dependencies:
             dependency_list = []
             for dependency in self.dependencies:
                 dependency_list.append(AtomicDependency(**dependency))
             self.dependencies = dependency_list
-        elif not self.dependencies and temp_prereq_command:
-            dependency_list = []
-            dependency_list.append(AtomicDependency(
-                description='',
-                prereq_command=temp_prereq_command
-            ))
-            self.dependencies = dependency_list
 
     def __replace_command_string(self, command: str):
         if command:
             # TODO: Figure out how to handle remote execution of these dependencies (e.g. T1037\\src\\batstartup.bat)
-            command = command.replace('$PathToAtomicsFolder', str(PurePath(Base.CONFIG.atomics_path)))
-            command = command.replace('PathToAtomicsFolder', str(Base.CONFIG.atomics_path))
+            try:
+                command = command.replace('$PathToAtomicsFolder', str(PurePath(Base.CONFIG.atomics_path)))
+                command = command.replace('PathToAtomicsFolder', str(PurePath(Base.CONFIG.atomics_path)))
+            except:
+                pass
             if self.input_arguments:
                 for input in self.input_arguments:
                     for string in self._replacement_strings:
@@ -117,9 +100,8 @@ class AtomicTest:
                 else:
                     arguments.value = arguments.default
         if self.executor:
-            for executor in self.executor:
-                executor.command = self.__replace_command_string(executor.command)
-                executor.cleanup_command = self.__replace_command_string(executor.command)
+            self.executor.command = self.__replace_command_string(self.executor.command)
+            self.executor.cleanup_command = self.__replace_command_string(self.executor.cleanup_command)
         if self.dependencies:
             for dependency in self.dependencies:
                 dependency.description = self.__replace_command_string(dependency.description)
