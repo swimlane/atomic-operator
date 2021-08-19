6d147963045fb5876c46f5d09024ee3c468b795a
msadministrator <rickardja@live.com>
2021-08-19 12:15:20 -0500

    Updated conftest to retrieve atomics using get_atomics method

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
+import atomic_operator
 import pytest
 
 
 @pytest.fixture
 def default_art_fixture():
     from atomic_operator import AtomicOperator
-    return AtomicOperator()
+    atomic_op = AtomicOperator()
+    atomic_op.get_atomics()
+    return atomic_op
