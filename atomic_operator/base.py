5ae059e60ed2d110620c628c2c8d5522cec6d045
msadministrator <rickardja@live.com>
2021-08-20 15:56:24 -0500

    Added and modified docstrings in base class

--- atomic_operator/base.py
+++ atomic_operator/base.py
@@ -36,19 +36,45 @@ class Base(metaclass=LoggingBase):
     }
 
     def clean_output(self, data):
+        """Decodes data and strips CLI garbage from returned outputs and errors
+
+        Args:
+            data (str): A output or error returned from subprocess
+
+        Returns:
+            str: A cleaned string which will be displayed on the console and in logs
+        """
         # Remove Windows CLI garbage
         data = re.sub(r"Microsoft\ Windows\ \[version .+\]\r?\nCopyright.*(\r?\n)+[A-Z]\:.+?\>", "", data)
         return re.sub(r"(\r?\n)*[A-Z]\:.+?\>", "", data)
 
     def download_atomic_red_team_repo(self, save_path, **kwargs):
+        """Downloads the Atomic Red Team repository from github
+
+        Args:
+            save_path (str): The path to save the downloaded and extracted ZIP contents
+
+        Returns:
+            str: A string of the location the data was saved to.
+        """
         response = requests.get(Base.ATOMIC_RED_TEAM_REPO, stream=True, **kwargs)
         z = zipfile.ZipFile(BytesIO(response.content))
         z.extractall(save_path)
         return z.namelist()[0]
 
     def format_config_data(self, config_file):
+        """Parses a provide path to a configuration data file (yaml).
+
+        Args:
+            config_file (str): A path to a valid config_file.
+
+        Raises:
+            FileNotFoundError: Raises if provided a config_file path that does not exist
             MalformedFile: Raises if the provided config file does not meet the defined format
 
+        Returns:
+            dict: A dictionary of configuration values used to drive atomic-operator execution of Atomics
+        """
         return_dict = {}
         if config_file:
             if not os.path.exists(config_file):
@@ -69,19 +95,39 @@ class Base(metaclass=LoggingBase):
         return return_dict
 
     def get_local_system_platform(self):
+        """Identifies the local systems operating system platform
+
+        Returns:
+            str: The current/local systems operating system platform
+        """
         os_name = platform.system().lower()
         if os_name == "darwin":
             return "macos"
         return os_name
 
     def get_abs_path(self, value):
+        """Formats and returns the absolute path for a path value
+
+        Args:
+            value (str): A path string in many different accepted formats
+
+        Returns:
+            str: The absolute path of the provided string
+        """
         return os.path.abspath(os.path.expanduser(os.path.expandvars(value)))
 
     def show_details(self, value):
+        """Displays the provided value string if Base.CONFIG.show_details is True
+
+        Args:
+            value (str): A string to display if selected in config.
+        """
         if Base.CONFIG.show_details:
             self.__logger.info(value)
 
     def prompt_user_for_input(self, title, input_object):
+        """Prompts user for input values based on the provided values.
+        """
         print(f"""
 Inputs for {title}:
     Input Name: {input_object.name}
@@ -94,16 +140,31 @@ Inputs for {title}:
             return value
         return input_object.default
 
-    def print_process_output(self, outs, errs):
-        # Output the appropriate outputs if they exist.
-        if outs:
-            self.__logger.info("Output: {}".format(self.clean_output(outs.decode("utf-8", "ignore"))))
+    def print_process_output(self, command, return_code, output, errors):
+        """Outputs the appropriate outputs if they exists to the console and log files
+
+        Args:
+            command (str): The command which was ran by subprocess
+            return_code (int): The return code from subprocess
+            output (bytes): Output from subprocess which is typically in bytes
+            errors (bytes): Errors from subprocess which is typically in bytes
+        """
         else:
             self.__logger.info("(No output)")
         if errs:
             self.__logger.error("Errors: {}".format(self.clean_output(errs.decode("utf-8", "ignore"))))
 
     def execute_subprocess(self, executor, command, cwd):
+        """Executes commands using subprocess
+
+        Args:
+            executor (str): An executor or shell used to execute the provided command(s)
+            command (str): The commands to run using subprocess
+            cwd (str): A string which indicates the current working directory to run the command
+
+        Returns:
+            tuple: A tuple of either outputs or errors from subprocess
+        """
         p = subprocess.Popen(
             executor, 
             shell=False, 
