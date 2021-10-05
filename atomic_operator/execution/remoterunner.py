d12a07635299d879babf29c91a4ca98642355dfb
msadministrator <rickardja@live.com>
2021-10-05 15:50:47 -0500

    Updated remoterunner

--- atomic_operator/execution/remoterunner.py
+++ atomic_operator/execution/remoterunner.py
@@ -1,9 +1,9 @@
 from rudder import Rudder
-from ..base import Base
+from .runner import Runner
 
 
-class RemoteRunner(Base):
-    
+class RemoteRunner(Runner):
+
     def __init__(self, atomic_test, test_path, runner):
         """A single AtomicTest object is provided and ran on the local system
 
@@ -16,40 +16,37 @@ class RemoteRunner(Base):
         self.test_path = test_path
         self.runner = runner
 
-    def execute_remote_process(self, command, override_executor=None) -> None:
+    def execute_process(self, command, executor=None, host=None, cwd=None):
         """Main method to execute commands using Rudder
 
         Args:
             command (str): The command to run remotely on the desired systems
-            override_executor (str, optional): An override executor that can be passed to rudder. Defaults to None.
+            executor (str): An executor that can be passed to rudder. Defaults to None.
+            host (str): A host to run remote commands on. Defaults to None.
         """
-        for runner in self.runner:
-            for response in Rudder().execute(
-                host=runner.hosts,
-                executor=override_executor if override_executor else runner.executor,
-                command=command
-            ):
-                self.__logger.info(response)
+        if not isinstance(host, list):
+            host = [host]
+        response = Rudder().execute(
+            host=host,
+            executor=executor,
+            command=command
+        )
+        self.__logger.debug(response)
+        if isinstance(response, dict):
+            for key,val in response.items():
+                return val
 
-    def __run_dependencies(self) -> None:
-        """Checking dependencies
-        """
-        if self.test.dependency_executor_name:
-            executor = self.test.dependency_executor_name
-        for dependency in self.test.dependencies:
-            self.show_details(f"Dependency description: {dependency.description}")
-            if Base.CONFIG.get_prereqs:
-                self.show_details(f"Retrieving prerequistes")
-                self.execute_remote_process(dependency.get_prereq_command, override_executor=executor)
-            self.execute_remote_process(dependency.prereq_command, override_executor=executor)
-
-    def run(self) -> None:
+    def run(self):
         """The main method which runs a single AtomicTest object remotely on one or more defined hosts.
         """
-        if Base.CONFIG.check_dependencies and self.test.dependencies:
-            self.__run_dependencies()
-        self.show_details("Running command")
-        self.execute_remote_process(self.test.executor.command)
-        if Base.CONFIG.cleanup and self.test.executor.cleanup_command:
-            self.show_details("Running cleanup command")
-            self.execute_remote_process(self.test.executor.cleanup_command)
+        return_dict = {}
+        for runner in self.runner:
+            for host in runner.hosts:
+                return_dict.update(
+                    self.execute(
+                        host_name=host.hostname, 
+                        executor=runner.executor, 
+                        host=host
+                    )
+                )
+        return return_dict
