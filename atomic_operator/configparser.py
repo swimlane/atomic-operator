00a70d2c38ee9d1f6d4ca810e690ecd0782aa016
msadministrator <rickardja@live.com>
2021-10-08 13:08:46 -0500

    Updated configparser

--- atomic_operator/configparser.py
+++ atomic_operator/configparser.py
@@ -1,13 +1,23 @@
+from logging import config
 import os
 from .base import Base
 from .utils.exceptions import MalformedFile
-
-from rudder import Host, Runner
+from .models import Host
 
 
 class ConfigParser(Base):
 
     __config_file = None
+    __techniques = []
+    __test_config = {}
+    __hosts = {}
+
+    def __init__(self, config_file=None):
+        if config_file:
+            self.config_file = self.__load_config(config_file)
+            self.__parse_test_guids()
+        else:
+            self.config_file = None
 
     def __load_config(self, config_file):
         if not os.path.exists(config_file):
@@ -36,15 +46,6 @@ class ConfigParser(Base):
             )
         return host_list
 
-    @property
-    def config_file(self):
-        return self.__config_file
-
-    @config_file.setter
-    def config_file(self, value):
-        if value:
-            self.__config_file = self.__load_config(value)
-
     def create_remote_host_object(self, 
         hostname=None,
         username=None,
@@ -64,6 +65,85 @@ class ConfigParser(Base):
                 port=ssh_port,
                 timeout=ssh_timeout
             )
+
+    def __parse_test_guids(self):
+        if self.config_file:
+            for item in self.config_file['atomic_tests']:
+                if item.get('guid'):
+                    if item['guid'] not in self.__test_config:
+                        self.__test_config[item['guid']] = []
+                    if item.get('inventories') and self.config_file.get('inventory'):
+                        # process inventories to run commands remotely
+                        for inventory in item['inventories']:
+                            if self.config_file['inventory'].get(inventory):
+                                self.__test_config[item['guid']] = self.__parse_hosts(self.config_file['inventory'][inventory])
+
+    def set_tests(self, guids=None, techniques=None, hosts=None, username=None, password=None,
+                            ssh_key_path=None,private_key_string=None, verify_ssl=False, 
+                            ssh_port=22, ssh_timeout=5
+        ):
+            if not isinstance(hosts, list):
+                hosts = [hosts]
+            if guids:
+                if not isinstance(guids, list):
+                    guids = [guids]
+                for guid in guids:
+                    if guid not in self.__test_config:
+                        self.__test_config[guid] = []
+                    for host in hosts:
+                        self.__test_config[guid].append(
+                            self.create_remote_host_object(
+                                hostname=host,
+                                username=username,
+                                password=password,
+                                ssh_key_path=ssh_key_path,
+                                private_key_string=private_key_string,
+                                verify_ssl=verify_ssl,
+                                ssh_port=ssh_port,
+                                ssh_timeout=ssh_timeout
+                            )
+                        )
+            if techniques:
+                if 'all' in techniques and not guids:
+                    if 'all' not in self.__test_config:
+                        self.__test_config['all'] = []
+                    for host in hosts:
+                        self.__test_config['all'].append(
+                            self.create_remote_host_object(
+                                hostname=host,
+                                username=username,
+                                password=password,
+                                ssh_key_path=ssh_key_path,
+                                private_key_string=private_key_string,
+                                verify_ssl=verify_ssl,
+                                ssh_port=ssh_port,
+                                ssh_timeout=ssh_timeout
+                            )
+                        )
+                elif 'all' not in techniques:
+                    for technique in techniques:
+                        if technique not in self.__test_config:
+                            self.__test_config[technique] = []
+                        for host in hosts:
+                            self.__test_config[technique].append(
+                                self.create_remote_host_object(
+                                    hostname=host,
+                                    username=username,
+                                    password=password,
+                                    ssh_key_path=ssh_key_path,
+                                    private_key_string=private_key_string,
+                                    verify_ssl=verify_ssl,
+                                    ssh_port=ssh_port,
+                                    ssh_timeout=ssh_timeout
+                                )
+                            )
+
+    @property
+    def config(self):
+        if self.__test_config:
+            return self.__test_config
+        else:
+            return None
 
     def is_defined(self, guid: str):
         if self.config_file:
@@ -104,10 +184,6 @@ class ConfigParser(Base):
                     for inventory in item['inventories']:
                         if self.config_file['inventory'].get(inventory):
                             return_list.append(
-                                Runner(
-                                    hosts=self.__parse_hosts(self.config_file['inventory'][inventory]),
-                                    executor=self.config_file['inventory'][inventory]['executor'],
-                                    command=''
-                                )
+                                self.__parse_hosts(self.config_file['inventory'][inventory])
                             )
         return return_list
