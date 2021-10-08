2fb15d975f4e10839911d2491582946c5f0ce2ff
msadministrator <rickardja@live.com>
2021-10-08 13:07:51 -0500

    Implemented statemachine in remote runner

--- atomic_operator/execution/remoterunner.py
+++ atomic_operator/execution/remoterunner.py
@@ -1,51 +1,81 @@
 from .runner import Runner
+from .statemachine import CreationState
+from paramiko.ssh_exception import (
+    BadAuthenticationType,
+    NoValidConnectionsError, 
+    AuthenticationException, 
+    PasswordRequiredException
+)
+from pypsrp.exceptions import (
+    AuthenticationError,
+    WinRMTransportError,
+    WSManFaultError
+)
 
 
 class RemoteRunner(Runner):
 
-    def __init__(self, atomic_test, test_path, runner):
+    def __init__(self, atomic_test, test_path):
         """A single AtomicTest object is provided and ran on the local system
 
         Args:
             atomic_test (AtomicTest): A single AtomicTest object.
             test_path (Atomic): A path where the AtomicTest object resides
-            runner (Runner): A Runner object defined with a config file
         """
         self.test = atomic_test
         self.test_path = test_path
-        self.runner = runner
 
     def execute_process(self, command, executor=None, host=None, cwd=None):
-        """Main method to execute commands using Rudder
+        """Main method to execute commands using state machine
 
         Args:
             command (str): The command to run remotely on the desired systems
-            executor (str): An executor that can be passed to rudder. Defaults to None.
+            executor (str): An executor that can be passed to state machine. Defaults to None.
             host (str): A host to run remote commands on. Defaults to None.
         """
-        if not isinstance(host, list):
-            host = [host]
-        response = Rudder().execute(
-            host=host,
-            executor=executor,
-            command=command
-        )
-        self.__logger.debug(response)
-        if isinstance(response, dict):
-            for key,val in response.items():
-                return val
+        self.state = CreationState()
+        final_state = None
+        try:
+            finished = False
+            while not finished:
+                if str(self.state) == 'CreationState':
+                    self.__logger.debug('Running CreationState on_event')
+                    self.state = self.state.on_event(executor, command)
+                if str(self.state) == 'InnvocationState':
+                    self.__logger.debug('Running InnvocationState on_event')
+                    self.state = self.state.invoke(host, executor, command)
+                if str(self.state) == 'ParseResultsState':
+                    self.__logger.debug('Running ParseResultsState on_event')
+                    final_state = self.state.on_event()
+                    self.__logger.info(final_state)
+                    finished = True
+        except NoValidConnectionsError as ec:
+            self.__logger.warning(f'SSH Error: Unable to connect to {host.hostname}: {ec}')
+            final_state = ec
+        except AuthenticationException as ea:
+            self.__logger.warning(f'SSH Error: Unable to authenticate to host {host.hostname}: {ea}')
+            final_state = ea
+        except BadAuthenticationType as eb:
+            self.__logger.warning(f'SSH Error: Unable to use provided authentication type to {host.hostname}: {eb}')
+            final_state = eb
+        except PasswordRequiredException as ep:
+            self.__logger.warning(f'SSH Error: Must provide a password to authenticate to {host.hostname}: {ep}')
+            final_state = ep
+        except AuthenticationError as ewa:
+            self.__logger.warning(f'Windows Error: Unable to authenticate to host {host.hostname}: {ewa}')
+            final_state = ewa
+        except WinRMTransportError as ewt:
+            self.__logger.warning(f'Windows Error: Error occurred during transport on host {host.hostname}: {ewt}')
+            final_state = ewt
+        except WSManFaultError as ewf:
+            self.__logger.warning(f'Windows Error: Received WSManFault information from host {host.hostname}: {ewf}')
+            final_state = ewf
+        except Exception as ex:
+            self.__logger.warning(f"Uknown Error: Received an unknown error from host {host.hostname}: {ex}")
+            final_state = ex
+        return final_state
 
-    def run(self):
-        """The main method which runs a single AtomicTest object remotely on one or more defined hosts.
+    def run(self, host=None, executor=None):
+        """The main method which runs a single AtomicTest object remotely on one remote host.
         """
-        return_dict = {}
-        for runner in self.runner:
-            for host in runner.hosts:
-                return_dict.update(
-                    self.execute(
-                        host_name=host.hostname, 
-                        executor=runner.executor, 
-                        host=host
-                    )
-                )
-        return return_dict
+        return self.execute(host_name=host.hostname, executor=executor, host=host)
