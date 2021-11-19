4f3150bbae1327a98f52fad481bbe20cf88ea594
msadministrator <rickardja@live.com>
2021-11-19 10:03:50 -0600

    Expanded tests

--- tests/test_art_loader.py
+++ tests/test_art_loader.py
@@ -1,12 +1,24 @@
 import os
 from pytest import raises
 from atomic_operator.atomic.loader import Loader
+from atomic_operator.atomic.atomic import Atomic
+from atomic_operator.atomic.atomictest import AtomicTest
 
 def test_loader_attribute_error():
     with raises(AttributeError):
         Loader().load_techniques()
 
 def test_load_technique():
-    data = Loader().load_technique(os.path.join(os.path.dirname(__file__), 'data', 'test_atomic.yml'))
+    path = os.path.join(os.path.dirname(__file__), 'data', 'test_atomic2.yml')
+    data = Loader().load_technique(path)
+    data.update({'path': path })
     assert isinstance(data, dict)
+    
 
+def test_convert_to_atomic_object():
+    path = os.path.join(os.path.dirname(__file__), 'data', 'test_atomic2.yml')
+    data = Loader().load_technique(path)
+    data.update({'path': path })
+    atomic = Atomic(**data)
+    assert isinstance(atomic, Atomic)
+    assert len(atomic.atomic_tests) >= 1
