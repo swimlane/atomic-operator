a948011aaa665d1904578e84dff802ea4a7b79fa
msadministrator <rickardja@live.com>
2021-12-17 10:22:27 -0600

    Added help as part of cmd line interface

--- atomic_operator/__main__.py
+++ atomic_operator/__main__.py
@@ -5,7 +5,8 @@ def main():
     atomic_operator = AtomicOperator()
     fire.Fire({
         'run': atomic_operator.run,
-        'get_atomics': atomic_operator.get_atomics
+        'get_atomics': atomic_operator.get_atomics,
+        'help': atomic_operator.help
     })
 
 if __name__ == "__main__":
