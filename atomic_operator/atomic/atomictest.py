0651607b381993ea512bcd759d85a8eff1054b95
MSAdministrator <rickardja@live.com>
2022-04-14 16:57:21 -0500

    Updated atomimc test model

--- atomic_operator/atomic/atomictest.py
+++ atomic_operator/atomic/atomictest.py
@@ -1,53 +1,31 @@
-import typing
-import attr
+from typing import (
+    AnyStr,
+    List
+)
+from attr import (
+    define,
+    field
+)
+from .base import (
+    TestBase,
+    Executor,
+    Dependency,
+    InputArguments
+)
 
 
-@attr.s
-class AtomicTestInput:
-
-    name        = attr.ib()
-    description = attr.ib()
-    type        = attr.ib()
-    default     = attr.ib()
-    value       = attr.ib(default=None)
-    source      = attr.ib(default=None)
-    destination = attr.ib(default=None)
-
-
-@attr.s
-class AtomicExecutor:
-
-    name                     = attr.ib()
-    command                  = attr.ib()
-    cleanup_command          = attr.ib(default=None)
-    elevation_required       = attr.ib(default=False)
-    steps                    = attr.ib(default=None)
-
-
-@attr.s
-class AtomicDependency:
-
-    description = attr.ib()
-    get_prereq_command = attr.ib(default=None)
-    prereq_command = attr.ib(default=None)
-
-
-@attr.s
-class AtomicTest:
+@define
+class AtomicTest(TestBase):
     """A single Atomic test object structure
 
     Returns:
         AtomicTest: A single Atomic test object
     """
-
-    name                                        = attr.ib()
-    description                                 = attr.ib()
-    supported_platforms                         = attr.ib()
-    auto_generated_guid                         = attr.ib()
-    executor                                    = attr.ib()
-    input_arguments                             = attr.ib(default=None)
-    dependency_executor_name                    = attr.ib(default=None)
-    dependencies: typing.List[AtomicDependency] = attr.ib(default=[])
+    supported_platforms:      AnyStr = field()
+    auto_generated_guid:      AnyStr = field()
+    input_arguments                  = field(default=None)
+    dependency_executor_name: AnyStr = field(default=None)
+    dependencies:             List[Dependency] = field(default=[])
 
     def __attrs_post_init__(self):
         if self.input_arguments:
@@ -56,19 +34,19 @@ class AtomicTest:
                 argument_dict = {}
                 argument_dict = val
                 argument_dict.update({'name': key, 'value': val.get('default')})
-                temp_list.append(AtomicTestInput(**argument_dict))
+                temp_list.append(InputArguments(**argument_dict))
             self.input_arguments = temp_list
         if self.executor:
             executor_dict = self.executor
             if executor_dict.get('name') == 'manual':
                 if not executor_dict.get('command'):
                     executor_dict['command'] = ''
-            self.executor = AtomicExecutor(**executor_dict)
+            self.executor = Executor(**executor_dict)
             executor_dict = None
         else:
             self.executor = []
         if self.dependencies:
             dependency_list = []
             for dependency in self.dependencies:
-                dependency_list.append(AtomicDependency(**dependency))
+                dependency_list.append(Dependency(**dependency))
             self.dependencies = dependency_list
