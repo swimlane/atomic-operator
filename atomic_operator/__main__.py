3b9e1fd42349c984d51e1266851060d841b1942b
MSAdministrator <rickardja@live.com>
2022-04-20 14:24:49 -0500

    Updated main and overall new entry point

--- atomic_operator/__main__.py
+++ atomic_operator/__main__.py
@@ -2,12 +2,7 @@ import fire
 from atomic_operator import AtomicOperator
 
 def main():
-    atomic_operator = AtomicOperator()
-    fire.Fire({
-        'run': atomic_operator.run,
-        'get_atomics': atomic_operator.get_atomics,
-        'help': atomic_operator.help
-    })
+    fire.Fire(AtomicOperator)
 
 if __name__ == "__main__":
     main()
