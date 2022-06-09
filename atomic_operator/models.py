42ab3c25a1a55828e029ffa2397075aa89a6f867
MSAdministrator <rickardja@live.com>
2022-06-09 09:43:58 -0500

    feat: Adding Configfile model

--- atomic_operator/models.py
+++ atomic_operator/models.py
@@ -61,3 +61,104 @@ class Config:
         value = self.__get_abs_path(value)
         if not os.path.exists(value):
             raise ContentFolderNotFound('Please provide a value for content_path that exists')
+
+
+@define
+class BaseFrameworkConfig:
+    content_path: DirectoryPath = field()
+
+
+@define
+class EmulationTestsConfig:
+    name: AnyStr = field()
+    input_arguments: dict = field(factory=dict)
+    inventories: List = field(factory=list)
+
+
+@define
+class AdversaryEmulationConfig(BaseFrameworkConfig):
+    tests: List[EmulationTestsConfig] = field()
+
+    def __attrs_post_init__(self):
+        if self.tests:
+            return_list = []
+            for item in self.tests:
+                return_list.append(EmulationTestsConfig(**item))
+            self.tests = return_list
+
+
+@define
+class AtomicTestsConfig:
+    guid: AnyStr = field()
+    input_arguments: dict = field(factory=dict)
+    inventories: List = field(factory=list)
+
+
+@define
+class AtomicConfig(BaseFrameworkConfig):
+    tests: List[AtomicTestsConfig]
+
+    def __attrs_post_init__(self):
+        if self.tests:
+            return_list = []
+            for item in self.tests:
+                return_list.append(AtomicTestsConfig(**item))
+            self.tests = return_list
+
+
+@define
+class ConfigFrameworks:
+    adversary_emulations: AdversaryEmulationConfig = field(default=dict)
+    atomic_tests: AtomicConfig = field(default=dict)
+
+    def __attrs_post_init__(self):
+        if self.adversary_emulations:
+            self.adversary_emulations = AdversaryEmulationConfig(**self.adversary_emulations)
+        if self.atomic_tests:
+            self.atomic_tests = AtomicConfig(**self.atomic_tests)
+    
+@define
+class InventoryConfig:
+    name: AnyStr = field()
+    hosts: list = field()
+    executor: AnyStr = field()
+
+
+@define
+class ConfigFile:
+    frameworks: ConfigFrameworks = field()
+    inventory: List[InventoryConfig] = field(factory=list)
+
+    def __attrs_post_init__(self):
+        if self.frameworks:
+            try:
+                self.frameworks = ConfigFrameworks(**self.frameworks)
+            except TypeError as te:
+                raise te
+        if self.inventory:
+            from .models import Host
+            return_list = []
+            for item in self.inventory:
+                host_list = []
+                for host in item.get('hosts'):
+                    host_list.append(
+                        Host(
+                            hostname=host,
+                            username=item.get('username'),
+                            password=item.get('password'),
+                            ssh_key_path=item.get('ssh_key_path'),
+                            private_key_string=item.get('private_key_string'),
+                            verify_ssl=item.get('verify_ssl', False),
+                            ssh_port=item.get('port') or item.get('ssh_port'),
+                            ssh_timeout=item.get('timeout') or item.get('ssh_timeout') 
+                        )
+                    )
+                return_list.append(
+                    InventoryConfig(
+                        name=item['name'],
+                        hosts=host_list,
+                        executor=item['executor']
+                    )
+                )
+            self.inventory = return_list
+
