a7f02d90fc37615fcd6172229936ce4a4da13e7e
MSAdministrator <rickardja@live.com>
2022-06-09 09:47:50 -0500

    feat: Updated AtomicTest framework data model

--- atomic_operator/frameworks/atomictest.py
+++ atomic_operator/frameworks/atomictest.py
@@ -1,11 +1,7 @@
-from typing import (
-    AnyStr,
-    List
-)
-from attr import (
-    define,
-    field
-)
+from typing import AnyStr, List
+
+from attr import define, field
+
 from .base import (
     TestBase,
     Executor,
@@ -16,17 +12,17 @@ from .base import (
 
 @define
 class AtomicTest(TestBase):
-    """A single Atomic test object structure
+    """A single Atomic test object structure.
 
     Returns:
         AtomicTest: A single Atomic test object
     """
-    supported_platforms:      AnyStr = field()
-    auto_generated_guid:      AnyStr = field()
-    executor: Executor = field()
-    input_arguments                  = field(default=None)
-    dependency_executor_name: AnyStr = field(default=None)
-    dependencies:             List[Dependency] = field(default=[])
+    supported_platforms:      AnyStr               = field()
+    auto_generated_guid:      AnyStr               = field()
+    executor:                 Executor             = field()
+    input_arguments:          List[InputArguments] = field(factory=list)
+    dependency_executor_name: AnyStr               = field(factory=str)
+    dependencies:             List[Dependency]     = field(factory=list)
 
     def __attrs_post_init__(self):
         if self.input_arguments:
@@ -51,3 +47,27 @@ class AtomicTest(TestBase):
             for dependency in self.dependencies:
                 dependency_list.append(Dependency(**dependency))
             self.dependencies = dependency_list
+
+    def get_command(self):
+        self._set_input_arguments()
+        if self.executor.command:
+            from ..base import Base
+            return self._replace_command_string(
+                command=self.executor.command,
+                path=Base.CONFIG.atomics_path,
+                executor=self.executor.name,
+                input_arguments=self.input_arguments,
+                elevation_required=self.executor.elevation_required
+            )
+
+    def get_cleanup_command(self):
+        self._set_input_arguments()
+        if self.executor.cleanup_command:
+            from ..base import Base
+            return self._replace_command_string(
+                command=self.executor.cleanup_command,
+                path=Base.CONFIG.atomics_path,
+                executor=self.executor.name,
+                input_arguments=self.input_arguments,
+                elevation_required=self.executor.elevation_required
+            )
