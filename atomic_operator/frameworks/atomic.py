f6ae8cf1545ac785f826afaa4a8b3662fba19421
MSAdministrator <rickardja@live.com>
2022-06-09 09:46:02 -0500

    feat: Updated Atomic framework data model

--- atomic_operator/frameworks/atomic.py
+++ atomic_operator/frameworks/atomic.py
@@ -1,26 +1,23 @@
-from typing import (
-    AnyStr,
-    List
-)
-from attr import (
-    define,
-    field,
-)
+from typing import AnyStr, List
+
+from attr import define, field
+
 from .atomictest import AtomicTest
+from .base import FrameworkBase
 from ..models import Host
 
 
 @define
-class Atomic:
-    """A single Atomic data structure. Each Atomic (technique)
-    will contain a list of one or more AtomicTest objects.
+class Atomic(FrameworkBase):
+    """A single Atomic data structure. 
+    
+    Each Atomic (technique) will contain a list of one or more AtomicTest objects.
     """
 
     attack_technique: AnyStr = field()
     display_name: AnyStr = field()
-    path: AnyStr = field()
     atomic_tests: List[AtomicTest] = field()
-    hosts: List[Host] = field(default=[])
+    hosts: List[Host] = field(factory=list)
 
     def __attrs_post_init__(self):
         if self.atomic_tests:
