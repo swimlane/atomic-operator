8a612a5a3c0272e9bb62ec5c3940e7cb6219ebc0
Brant <brantw@gmail.com>
2022-06-15 19:49:17 +0200

    Updating code comment docs

--- atomic_operator/atomic_operator.py
+++ atomic_operator/atomic_operator.py
@@ -34,7 +34,7 @@ class AtomicOperator(Base):
                     executor: ssh
                     authentication:
                       username: root
-                      password: UR4Swimlane!
+                      password: Somepassword!
                       #ssk_key_path:
                       port: 22
                       timeout: 5
